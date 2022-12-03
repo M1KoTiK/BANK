4dc63e134645548b52a3643cb34be40b03df35b3
M1KoTiK <temhbz@mail.ru>
2022-12-03 13:32:45 +0700

    Almost finished 3rd screen
    Well, like i did smth

--- BANK_Kargin/obj/Debug/BANK_Kargin_Content.g.i.cs
+++ BANK_Kargin/obj/Debug/BANK_Kargin_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ph1.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("view/ph1.png")]
 
 
